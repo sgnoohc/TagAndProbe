0dcd3e3ea0109d36582e9b3fc5556437a2bbc23f
gzevi <giovanni.zevidellaporta@gmail.com>
2015-08-03 09:41:42 -0700

    clean before copying files

--- Analysis/tools/makeWebsite.sh
+++ Analysis/tools/makeWebsite.sh
@@ -18,18 +18,18 @@ rm $wwwDir/MC/* $wwwDir/Data/* $wwwDir/*.png
 cp $locDir/compare/index.php $wwwDir/
 cp $locDir/compare/index.php $wwwDir/Data
 cp $locDir/compare/index.php $wwwDir/MC
-cp $locDir/compare/p_eff_pt_eta* $wwwDir/
+cp $locDir/compare/p_eff_* $wwwDir/
 cp $locDir/compare/p_sf*.png $wwwDir/
 cp $locDir/compare/eff_tables.txt $wwwDir/
    
-cp $locDir/data_*_el_eff/png/p_*_pt*vs*eta* $wwwDir/Data
-cp $locDir/dy_*_eff/png/p_*_pt*vs*eta* $wwwDir/MC
+cp $locDir/data_*_el_eff/png/p_*vs* $wwwDir/Data
+cp $locDir/dy_*_eff/png/p_*vs* $wwwDir/MC
 
 cd ~/www/public_html/
 rm webtest.zip
 zip -r webtest.zip $wwwName/*
 scp webtest.zip $USER@lxplus.cern.ch:~/www/public_html/
-ssh $USER@lxplus.cern.ch "cd  www/public_html/; unzip webtest.zip;"
+ssh $USER@lxplus.cern.ch "cd  www/public_html/; rm -rf ${wwwName}; unzip webtest.zip;"
 cd -
 
 
